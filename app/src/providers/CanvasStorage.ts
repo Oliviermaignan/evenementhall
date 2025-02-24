117c709115bcdec9287c9ffc235c62b85bc7b937
oliviermaignan <ol.maignan@gmail.com>
2025-02-24 15:55:23 +0100

    feat: new interface for ShapesConfig storage

--- app/src/providers/CanvasStorage.ts
+++ app/src/providers/CanvasStorage.ts
@@ -1,5 +1,17 @@
+// interface a def TODO
+export interface ShapesConfig {
+  name: string
+  id: number
+  x: number
+  y: number
+  angle: number
+  icon: string
+}
+
+
+
 export abstract class CanvasStorage {
-    abstract save(data: string): void;
+    abstract save(data: ShapesConfig): void;
     abstract load(): string | null;
   }
   
