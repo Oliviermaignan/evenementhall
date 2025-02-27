c42ad140edeebcdc5459dd13c4b8c059d699d79e
oliviermaignan <ol.maignan@gmail.com>
2025-02-27 15:53:28 +0100

    feat: new test for storage

--- app/src/test/storage.test.ts
+++ app/src/test/storage.test.ts
@@ -1,6 +1,21 @@
-import { expect, test } from 'vitest'
+import { describe,it,vi,expect,beforeEach } from "vitest";
+import Shape from "../components/ShapeClass";
+import CanvasLocalStorage from "../adapters_out/CanvasLocalStorage";
+import { CanevasIconName } from "../components/enums";
 
+describe("CanvasLocalStorage",() => {
+    // pour que ce test passe j'ai du créer un fichier vitest.config.ts
+    const key = "oui!";
+    it("should store the array of shapes in localStorage",() => {
 
-test('first test', () => {
-  expect(true).toBe(true)
-})
+        const mockCanvas = document.createElement("canvas");
+        const shape: Shape = new Shape(mockCanvas,0,0,0,CanevasIconName.Chaise)
+        const shapes: Shape[] = [shape,shape]
+
+        const canvasLocalStorage = new CanvasLocalStorage(key);
+
+        canvasLocalStorage.save(shapes);
+
+        expect(localStorage.getItem(key)).toEqual(JSON.stringify(shapes));
+    });
+});
